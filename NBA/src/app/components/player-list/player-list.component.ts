7bd797391440beda3be3cdfef799bae78a2ca71a
illoquehambre <illoquehambre@gmail.com>
2022-10-21 10:18:09 +0200

    necesario pal merge

--- NBA/src/app/components/player-list/player-list.component.ts
+++ NBA/src/app/components/player-list/player-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Player, } from 'src/app/interfaces/playersList.interface';
+import { PlayersService } from 'src/app/services/players.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-player-list',
@@ -6,10 +9,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./player-list.component.css']
 })
 export class PlayerListComponent implements OnInit {
+  currentYear:number =new Date().getFullYear();
+  playerList: Player[]=[]
+  yearList:number[]=[]
+  yearSelected: number = {} as number;
+  numPages=0
 
-  constructor() { }
+  constructor(
+    private playersService: PlayersService
+  ) { }
 
   ngOnInit(): void {
+
+    this.getPlayersPage(1, this.currentYear )
+
+    for (let i = 0; 2012+i<=this.currentYear; i++) {
+      this.yearList[i]=(this.currentYear)-i;      
+    }
   }
 
+  getPlayersPage(page: number, year: number){
+    let count=0
+    this.playersService.getPlayerList(year, page).subscribe(resp =>{
+      this.playerList=resp.league.standard;
+      this.playerList.forEach(player => {
+        count++      
+      });
+      this.numPages=Math.ceil(count/20)
+    })
+  }
+
+  selectYear() {
+    if(this.yearSelected !== null){
+      this.playersService.getPlayerList(this.yearSelected, this.numPages).subscribe(resp =>{
+        this.playerList = resp.league.standard;
+      })
+    }
+   
+
+  }
+  showImgPlayer(player: Player) {
+    let id = player.personId;
+    return `${environment.API_IMG_PLAYER_URL}/${id}.png`;
+  }
+  counter(){
+    return new Array(this.numPages)
+  }
 }
